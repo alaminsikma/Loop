2ea8c2ff2533f65217ce935e4c886d9038abbea9
alaminsikma <alaminpratama9@gmail.com>
2026-09-23 14:08:24 +0700

    loading made

--- EnemyWave/Program.cs
+++ EnemyWave/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿for (int i= 0;i<100;i++)
+{
+    Console.WriteLine($"Wave {i} muncul ");
+    Console.WriteLine($"wave {i} declear\n");
+}
+
+
+string name = "al amin";
+
+for (int a = 1; a <= 5; a++)
+{
+    Console.WriteLine($"Namaku Adalah : {name}");
+}
---
+++ LoadingScreen/LoadingScreen/Program.cs
@@ -0,0 +1,8 @@
+﻿int totalSteps = 33;
+
+        for (int step = 0; step <= totalSteps; step += 3)
+        {
+            Console.WriteLine($"Loading... Step {step} of {totalSteps}");
+        }
+
+        Console.WriteLine("\nDungeon is ready. Enter now!");
