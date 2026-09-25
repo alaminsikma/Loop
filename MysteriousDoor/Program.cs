f32912c55f80ba7012704303f197eea12b689e28
alaminsikma <alaminpratama9@gmail.com>
2026-09-25 14:03:19 +0700

    MysteriousDoor

---
+++ MysteriousDoor/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hello, World!");
+
+string secretCode = "26";
+string attempt = "";
+
+while(attempt != secretCode)
+{
+    Console.WriteLine("Enter The Secret Code : ");
+    attempt = Console.ReadLine();
+    if(attempt!= secretCode)
+    Console.WriteLine("kamu salah");
+}
+Console.WriteLine("sangar cyak arek tepak");
